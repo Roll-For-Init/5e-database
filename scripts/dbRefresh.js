911e8a01ad319e8f76d4d7b1fcb6ee82c7306afb
Michaela <m.isaacs@ufl.edu>
2021-04-04 22:42:41 -0400

    altered sorcerous origin dragon descs and names

--- scripts/dbRefresh.js
+++ scripts/dbRefresh.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 const fs = require('fs');
 
 // check the environment variable is set
-const mongodb_uri = {insertUriHere};
+const mongodb_uri = "mongodb+srv://admin:adminpass@rollforinit.xhpae.mongodb.net/ruleset?retryWrites=true&w=majority";
 if (mongodb_uri === undefined) {
     console.error("uri undefined");
     const localhost_uri_example = 'mongodb://localhost/5e-database';
